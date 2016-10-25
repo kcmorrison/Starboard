5f74210c8e5e7982969f6b7811ffdd81ceaa48a0
Kyle Morrison <kylewd2@gmail.com>
2016-10-25 03:16:07 -0400

    Cleanup

--- Assets/Scripts/ShipAnimation.cs
+++ Assets/Scripts/ShipAnimation.cs
@@ -10,47 +10,6 @@ public class ShipAnimation : MonoBehaviour {
     void Update()
     {
 
-        //if (ShipMovement.movementSpeed == 0)
-        //{
-        //    anim = GetComponent<Animation>();
-        //    print("frame " + anim["Take 001"].time + "");
-        //    anim["Take 001"].time = 1.6f;
-        //    anim["Take 001"].speed = 0f;
-        //}
-
-        //else if (ShipMovement.movementSpeed > 0 && anim["Take 001"].time > 1f)
-        //{
-        //    anim = GetComponent<Animation>();
-        //    print("frame " + anim["Take 001"].time + "");
-        //    anim["Take 001"].time -= .01f;
-        //    anim["Take 001"].speed = 0f;
-        //}
-
-        //else if (ShipMovement.movementSpeed > 0 && anim["Take 001"].time == 1.000001f)
-        //{
-        //    anim = GetComponent<Animation>();
-        //    print("frame " + anim["Take 001"].time + "");
-        //    anim["Take 001"].time = 0.1f;
-        //    anim["Take 001"].speed = 0f;
-        //}
-
-        //else if (ShipMovement.movementSpeed > 0 && anim["Take 001"].time > 0f && anim["Take 001"].time < .91f)
-        //{
-        //    anim = GetComponent<Animation>();
-        //    print("frame " + anim["Take 001"].time + "");
-        //    anim["Take 001"].time += .005f;
-        //    anim["Take 001"].speed = 0f;
-        //}
-
-        //else if (ShipMovement.movementSpeed > 0 && anim["Take 001"].time == 0.9099993f && anim["Take 001"].time != 0.1f)
-        //{
-        //    anim = GetComponent<Animation>();
-        //    print("frame " + anim["Take 001"].time + "");
-        //    anim["Take 001"].time = 0.1f;
-        //    anim["Take 001"].speed = 0f;
-        //}
-
-
         if (ShipMovement.movementSpeed == 0)
         {
             anim = GetComponent<Animation>();
